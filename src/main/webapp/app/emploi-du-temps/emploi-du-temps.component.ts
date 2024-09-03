28d616e8b1fbb6090b0ca7ef6828f22c5245643e
elgymaralaw <elhadjiibrahima.lo@itd-hub.com>
2024-09-03 16:12:59 +0000

    le chemin des emplois du temps

---
+++ src/main/webapp/app/emploi-du-temps/emploi-du-temps.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'jhi-emploi-du-temps',
+  standalone: true,
+  imports: [],
+  templateUrl: './emploi-du-temps.component.html',
+  styleUrl: './emploi-du-temps.component.scss'
+})
+export class EmploiDuTempsComponent {
+
+}
